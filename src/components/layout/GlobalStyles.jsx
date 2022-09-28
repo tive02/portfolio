1c78f37370892de06da6f46beb19343b9f37fbcb
tive02 <jsmorenop@unal.edu.co>
2022-09-27 21:16:01 -0500

    add background color

--- src/components/layout/GlobalStyles.jsx
+++ src/components/layout/GlobalStyles.jsx
@@ -5,9 +5,9 @@ const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    background: #17202A;
+    background: #1e1e1e;
     font-family: Open-Sans, Helvetica, Sans-Serif;
-  }
+}
 `;
  
 export default GlobalStyle;
--- src/components/molecules/CardMain.jsx
+++ src/components/molecules/CardMain.jsx
@@ -1,5 +1,5 @@
-
 import Foto from "../atoms/FotoMed.png";
+
 
 
 const CardMain = () => {
@@ -10,7 +10,7 @@ const CardMain = () => {
       Hi, Soy Steven Moreno  
       </h1>
       <img src={Foto} alt="Foto Jorge Moreno" />
-      
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dignissim nisi ut justo consequat, ut congue magna laoreet. Nulla arcu sem, efficitur id varius id, semper sit amet nisi. Maecenas volutpat blandit velit ut tempus. Duis vitae lacus mollis, eleifend massa eu, mattis mi. Mauris quis metus quis nisi hendrerit hendrerit vestibulum sagittis metus. Nam volutpat varius ante nec mollis. Nullam efficitur, arcu non blandit ornare, enim elit viverra ligula, gravida tincidunt mauris diam pellentesque felis. Sed vitae vehicula turpis, ac vulputate leo. Vestibulum sodales, dolor a vehicula laoreet, odio mauris imperdiet felis, in sodales ante risus dignissim mi. Etiam quis purus non diam posuere molestie in ut ex. Sed elementum augue et metus egestas, vitae luctus diam scelerisque. Fusce lorem tellus, posuere et hendrerit eu, hendrerit ac dui. Nulla dictum leo libero.</p>
     </div>
   );
 };
